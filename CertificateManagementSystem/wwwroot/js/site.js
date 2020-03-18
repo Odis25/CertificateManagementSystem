f4a65ef7f134995dcf86309152fa00bedbbd82ff
Odis <tracemode@mail.ru>
2020-03-18 21:25:30 +0300

    автозаполнение

--- CertificateManagementSystem/wwwroot/js/site.js
+++ CertificateManagementSystem/wwwroot/js/site.js
@@ -3,7 +3,33 @@
 
 // Write your JavaScript code.
 jQuery.ready(function () {
-
+    let availableTags = [
+        "ActionScript",
+        "AppleScript",
+        "Asp",
+        "BASIC",
+        "C",
+        "C++",
+        "Clojure",
+        "COBOL",
+        "ColdFusion",
+        "Erlang",
+        "Fortran",
+        "Groovy",
+        "Haskell",
+        "Java",
+        "JavaScript",
+        "Lisp",
+        "Perl",
+        "PHP",
+        "Python",
+        "Ruby",
+        "Scala",
+        "Scheme"
+    ];
+    $('#inputExploitationPlace').autocomplete({
+        source: availableTags
+    });
 });
 
 
