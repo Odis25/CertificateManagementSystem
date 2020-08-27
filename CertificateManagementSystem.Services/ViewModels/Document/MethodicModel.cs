f78200bc7f53160d871856c540ccb113f5f444b8
Odis Odis <tracemode@mail.ru>
2020-08-27 15:41:48 +0300

    рефакторинг

--- CertificateManagementSystem.Services/ViewModels/Document/MethodicModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace CertificateManagementSystem.Services.ViewModels.Document
-{
-    public class MethodicModel
-    {
-        public string Name { get; set; }
-        public string FileName { get; set; }
-    }
-}
